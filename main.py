b4ef6e528f055d638a4d0732ddc17f67733f7ede
Osmar <osmarcc70@gmail.com>
2026-04-09 15:39:35 -0600

    Cambio nombre

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 from app.api.router import api_router
 from app.exceptions import register_exception_handlers
 
-app = FastAPI(title="API Escolar", version="1.0.0")
+app = FastAPI(title="AWS Alumnos", version="1.0.0")
 register_exception_handlers(app)
 app.include_router(api_router)
